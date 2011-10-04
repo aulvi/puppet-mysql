dbb01c0699dc378a24f9754582855050c6d4cf1b
nerdlich <thomas.sturm@yahoo.de>
2011-10-04 12:54:04 +0300

    Edited manifests/definitions/rights.pp via GitHub

--- manifests/definitions/rights.pp
+++ manifests/definitions/rights.pp
@@ -23,9 +23,10 @@ Available parameters:
 */
 define mysql::rights($database, $user, $password, $host="localhost", $ensure="present", $priv="all") {
 
-  if $mysql_exists == "true" and $ensure == "present" {
+  if $mysql_exists == "true" {
     if ! defined(Mysql_user ["${user}@${host}"]) {
       mysql_user { "${user}@${host}":
+        ensure => $ensure,
         password_hash => mysql_password($password),
         require => File["/root/.my.cnf"],
       }
