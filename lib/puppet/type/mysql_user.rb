75bc9b1d8c19b49b8d008e1d127752fb14f73c9a
Corey Frang <gnarf37@gmail.com>
2014-02-01 20:45:55 -0500

    remove username check to get rid of super call altogether

--- lib/puppet/type/mysql_user.rb
+++ lib/puppet/type/mysql_user.rb
@@ -5,15 +5,6 @@ Puppet::Type.newtype(:mysql_user) do
   ensurable
   newparam(:name) do
     desc "The name of the user. This uses the 'username@hostname' form."
-
-    validate do |value|
-      if value.split('@').first.size > 16
-        raise ArgumentError,
-              "MySQL usernames are limited to a maximum of 16 characters"
-      else
-        super()
-      end
-    end
   end
 
   newproperty(:password_hash) do
