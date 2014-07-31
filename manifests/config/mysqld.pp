51744c3d42126c407e5e65ceac217950aa705148
Corey Frang <gnarf37@gmail.com>
2014-07-30 21:10:11 -0400

    remove log things that are breaking on new mysql

--- manifests/config/mysqld.pp
+++ manifests/config/mysqld.pp
@@ -5,14 +5,14 @@ class mysql::config::mysqld {
     'character-set-server': value => 'utf8';
     'log-warnings':         value => '1';
     'datadir':              value => "${mysql::params::data_dir}";
-    'log-error':            value => $::operatingsystem ? {
-      /RedHat|Fedora|CentOS/ => '/var/log/mysqld.log',
-      default                => '/var/log/mysql.err',
-      };
-    'log-slow-queries':     value => $::operatingsystem ? {
-      /RedHat|Fedora|CentOS/ => '/var/log/mysql-slow-queries.log',
-      default                => '/var/log/mysql/mysql-slow.log',
-      };
+    # 'log-error':            value => $::operatingsystem ? {
+    #   /RedHat|Fedora|CentOS/ => '/var/log/mysqld.log',
+    #   default                => '/var/log/mysql.err',
+    #   };
+    # 'log-slow-queries':     value => $::operatingsystem ? {
+    #   /RedHat|Fedora|CentOS/ => '/var/log/mysql-slow-queries.log',
+    #   default                => '/var/log/mysql/mysql-slow.log',
+    #   };
     # "ins log-slow-admin-statements after log-slow-queries", # BUG: not implemented in puppet yet
     'socket':                value => $::operatingsystem ? {
       /RedHat|Fedora|CentOS/ => '/var/lib/mysql/mysql.sock',
